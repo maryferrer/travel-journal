258fd1f886ada611daa2769488267129c74f6148
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-27 18:56:42 -0800

    header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <div className="header">
+        <i class="fas fa-suitcase"></i>
+        <h1 className="header--title">my travel journal</h1>
+        </div>
+    )
+}
+
+export default Header;
