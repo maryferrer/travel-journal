b0f2253b878393e4be0791ccfcc973667f52abea
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-27 18:57:31 -0800

    data being referenced in card component

---
+++ src/Data.js
@@ -0,0 +1,36 @@
+const array = [
+    {
+        id: 1,
+        img: "/wedding-photography.png",
+        alt: "wedding",
+        country: "JAPAN",
+        maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
+        location: "Mount Fuji",
+        date: "12 Jan, 2021 - 24 Jan 2021",
+        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.",
+        favePart: "placeholder",
+        faveFood: "placeholder food"
+    },
+    {
+        id: 2,
+        img: "/wedding-photography.png",
+        alt: "wedding",
+        country: "Japan",
+        maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
+        location: "Mt. Fuji",
+        date: "12 Jan, 2021 - 24 Jan 2021",
+        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige."
+    },
+    {
+        id: 3,
+        img: "/wedding-photography.png",
+        alt: "wedding",
+        country: "Japan",
+        maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
+        location: "Mt. Fuji",
+        date: "12 Jan, 2021 - 24 Jan 2021",
+        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige."
+    }
+]
+
+export default array;
