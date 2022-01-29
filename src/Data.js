bdf11466b5fb96e8cf15177082f088fc446e3f10
maryferrer <85918290+maryferrer@users.noreply.github.com>
2022-01-28 20:47:19 -0800

    update Data.js with travel details

--- src/Data.js
+++ src/Data.js
@@ -2,34 +2,38 @@ const array = [
     {
         id: 1,
         img: "/wedding-photography.png",
-        alt: "wedding",
-        country: "JAPAN",
-        maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
-        location: "Mount Fuji",
-        date: "12 Jan, 2021 - 24 Jan 2021",
-        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige.",
-        favePart: "placeholder",
-        faveFood: "placeholder food"
+        alt: "Fort Collins",
+        country: "USA",
+        maps: "https://goo.gl/maps/U2SPLyBHiX86rERQ6",
+        location: "Fort Collins, CO",
+        date: "20 Dec, 2021 - 27 Dec 2021",
+        description: "Fort Collins is a sunny town located about an hour and a half north of Denver and an hour east of Rocky Mountain National Park. At the heart of the city is Old Town--a bustling downtown center filled with locally-owned stores, restaurants and multitudes of activities. Fort Collins is truly one of the cutest cities that you've never heard of.",
+        favePart: "Mountain biking at Horsetooth Reservoir",
+        faveFood: "Vatos Tacos + Tequila, Old Town"
     },
     {
         id: 2,
         img: "/wedding-photography.png",
-        alt: "wedding",
-        country: "Japan",
+        alt: "San Francisco",
+        country: "USA",
         maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
-        location: "Mt. Fuji",
-        date: "12 Jan, 2021 - 24 Jan 2021",
-        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige."
+        location: "San Francisco",
+        date: "28 Oct, 2021 - 29 Oct, 2021",
+        description: "San Francisco has and always will be my favorite city. It was the first city I lived in when my family immigrated to the United States and one that holds many memories for us. San Francisco is bursting with culture. Whether its food, music or art that you're looking for, you're sure to find a community in SF.",
+        favePart: "Outside Lands Music Festival",
+        faveFood: "Uncle Boy's Burgers"
     },
     {
         id: 3,
         img: "/wedding-photography.png",
         alt: "wedding",
-        country: "Japan",
+        country: "USA",
         maps: "https://goo.gl/maps/TXoa9fNLnYEFkAco9",
-        location: "Mt. Fuji",
-        date: "12 Jan, 2021 - 24 Jan 2021",
-        description: "Japan’s Mt. Fuji is an active volcano about 100 kilometers southwest of Tokyo. Commonly called “Fuji-san,” it’s the country’s tallest peak, at 3,776 meters. A pilgrimage site for centuries, it’s considered one of Japan’s 3 sacred mountains, and summit hikes remain a popular activity. Its iconic profile is the subject of numerous works of art, notably Edo Period prints by Hokusai and Hiroshige."
+        location: "Yosemite National Park",
+        date: "20 Dec, 2021 - 27 Dec 2021",
+        description: "Have you ever visited a place and felt like you could stay forever? As if being in that place completes you and once you leave, a little piece of you will always linger there? That's how I feel about Yosemite National Park. I've visited countless times with family, friends, partners and every time the experience leaves me wondering if I should leave all my wordly posessions and responsibilities behind to stay within the comfort of that magical valley. This time around, I went with a mission. This time around I was one of the lucky few to score a Half Dome permit. This time around I would summit that iconic slab of rock.",
+        favePart: "Hiking Half Dome",
+        faveFood: "Dengnan's Deli"
     }
 ]
 
